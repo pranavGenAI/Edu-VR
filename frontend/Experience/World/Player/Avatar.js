4ed1e0e5c6c6904b47c2138f1e4f820181f46ed8
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-08-27 09:45:11 +0530

    Avatar.js

--- frontend/Experience/World/Player/Avatar.js
+++ frontend/Experience/World/Player/Avatar.js
@@ -1,7 +1,6 @@
 import * as THREE from "three";
 import * as SkeletonUtils from "three/addons/utils/SkeletonUtils.js";
 import Nametag from "./Nametag.js";
-
 export default class Avatar {
     constructor(avatar, scene, name = "Anonymous", id) {
         this.scene = scene;
@@ -9,14 +8,11 @@ export default class Avatar {
         this.nametag = this.name.createNametag(16, 150, name);
         this.avatar = SkeletonUtils.clone(avatar.scene);
         this.avatar.userData.id = id;
-
         this.avatar.animations = avatar.animations.map((clip) => {
             return clip.clone();
         });
-
         this.setAvatar();
     }
-
     setAvatar() {
         this.speedAdjustment = 1;
         this.avatar.scale.set(0.99, 0.99, 0.99);
@@ -27,25 +23,19 @@ export default class Avatar {
             this.scene.add(this.nametag);
         }
     }
-
     setAnimation() {
         this.animation = {};
-
         this.animation.mixer = new THREE.AnimationMixer(this.avatar);
-
         this.animation.actions = {};
-
         this.animation.actions.dancing = this.animation.mixer.clipAction(
             this.avatar.animations[0]
         );
-
         this.animation.actions.idle = this.animation.mixer.clipAction(
             this.avatar.animations[1]
         );
         this.animation.actions.jumping = this.animation.mixer.clipAction(
             this.avatar.animations[2]
         );
-
         this.animation.actions.running = this.animation.mixer.clipAction(
             this.avatar.animations[3]
         );
@@ -55,31 +45,24 @@ export default class Avatar {
         this.animation.actions.waving = this.animation.mixer.clipAction(
             this.avatar.animations[5]
         );
-
         this.animation.actions.current = this.animation.actions.idle;
         this.animation.actions.current.play();
-
         this.animation.play = (name) => {
             const newAction = this.animation.actions[name];
             const oldAction = this.animation.actions.current;
-
             if (oldAction === newAction) {
                 return;
             }
-
             if (this.animation.actions.current === "jumping") {
                 this.speedAdjustment = 1.5;
             } else {
                 this.speedAdjustment = 1.0;
             }
-
             newAction.reset();
             newAction.play();
             newAction.crossFadeFrom(oldAction, 0.2);
-
             this.animation.actions.current = newAction;
         };
-
         this.animation.update = (time) => {
             this.animation.mixer.update(time * this.speedAdjustment);
         };
