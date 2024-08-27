037477e8dc79153afcda6842fe2402b671459f4f
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-08-27 09:46:01 +0530

    Environment.js

--- frontend/Experience/World/Environment.js
+++ frontend/Experience/World/Environment.js
@@ -9,10 +9,9 @@ export default class Environment {
 
         this.setEnvironment();
     }
-
     setEnvironment() {
         this.environmentMap = {};
-        this.environmentMap.intensity = 0;
+        this.environmentMap.intensity = 1;
         this.environmentMap.texture = this.resources.items.environment;
         this.environmentMap.texture.outputColorSpace = THREE.SRGBColorSpace;
 
@@ -25,30 +24,6 @@ export default class Environment {
 
         this.sunLight.position.set(1.5, 7, -3);
         this.scene.add(this.sunLight);
-
-        // this.scene.environment = this.environmentMap.texture;
-
-        // console.log(this.scene);
-
-        // this.environmentMap.updateMaterials = () => {
-        //     this.scene.children.forEach((child) => {
-        //         if (child instanceof THREE.Group) {
-        //             console.log(child.children[0]);
-        //             if (
-        //                 child.children[0] instanceof THREE.Mesh &&
-        //                 child.children[0].material instanceof
-        //                     THREE.MeshPhysicalMaterial
-        //             ) {
-        //                 child.children[0].material.envMap =
-        //                     this.environmentMap.texture;
-        //                 child.children[0].material.envMapIntensity =
-        //                     this.environmentMap.intensity;
-        //                 child.children[0].material.needsUpdate = true;
-        //             }
-        //         }
-        //     });
-        // };
-        // this.environmentMap.updateMaterials();
     }
 
     update() {}
